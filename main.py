d4b2b51203b357547faa3babad88b56703d92167
alexrazor <alexrazor1337@gmail.com>
2020-11-29 13:19:46 +0200

    Iterating through objects

--- main.py
+++ main.py
@@ -3,12 +3,17 @@
 # -*- coding: utf-8 -*-
 
 import os, sys
+from lxml import etree
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         for element in sys.argv[1::]:
             if os.path.isfile(element):
-                pass #TODO
+                with open(element) as file:
+                    file_string = file.read().replace('edf:', '') # Parser doesn't like namespaces
+                    root = etree.fromstring(file_string)
+                    for obj in root.xpath("object"):
+                        pass # TODO
             else:
                 print("ERROR:", element, "is not a file!")
     else:
